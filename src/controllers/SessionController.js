7acc0ccc68ad25e87193a492418cfbe942e32148
William Vitorino <williamvitorino3@gmail.com>
2020-03-26 16:34:22 -0300

    Update na regra de negócio do Login da ONG.

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -3,10 +3,11 @@ const connection = require('../database/connection');
 async function create (req, res) {
 
 	const { id } = req.body;
+	console.log(id);
 
 	connection('ongs').where('id', id).select('name').first().then(
 		ong => {
-			return res.json(ong);
+			return !!ong ? res.json(ong) : res.status(400).json({ err });
 		}
 	).catch(
 		err => {
