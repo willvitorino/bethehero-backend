d66d207c1594127478e3470c562e901ac19c9a0c
William Vitorino <williamvitorino3@gmail.com>
2020-03-26 16:37:40 -0300

    Correção na mensagem de erro.

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -3,15 +3,14 @@ const connection = require('../database/connection');
 async function create (req, res) {
 
 	const { id } = req.body;
-	console.log(id);
 
 	connection('ongs').where('id', id).select('name').first().then(
 		ong => {
-			return !!ong ? res.json(ong) : res.status(400).json({ err });
+			return !!ong ? res.json(ong) : res.status(400).json({ error: "ID inexistente." });
 		}
 	).catch(
 		err => {
-			return res.status(400).json({ err });
+			return res.status(400).json({ error: "ID inexistente." });
 		}
 	);
 }
