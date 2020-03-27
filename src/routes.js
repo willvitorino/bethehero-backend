3b59ad46cb331d7879ee9d1f5716b277922cc125
William Vitorino <williamvitorino3@gmail.com>
2020-03-27 15:04:08 -0300

    Sem ; no final dos comandos

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,31 @@
-const express = require('express');
+const express = require('express')
+const {  } = require('celebrate')
 
-const OngController = require('./controllers/OngController');
-const IncidentController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController');
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
-const routes = express.Router();
+const routes = express.Router()
 
 /*
 * Login
 * */
-routes.post('/v1/sessions', SessionController.create);
+routes.post('/v1/sessions', SessionController.create)
 
 /*
 * Ongs
 * */
-routes.post('/v1/ongs', OngController.create);
-routes.get('/v1/ongs', OngController.index);
+routes.post('/v1/ongs', OngController.create)
+routes.get('/v1/ongs', OngController.index)
 
 /*
 * Incidents
 * */
-routes.post('/v1/incidents', IncidentController.create);
-routes.get('/v1/incidents', IncidentController.index);
-routes.delete('/v1/incidents/:id', IncidentController.remove);
+routes.post('/v1/incidents', IncidentController.create)
+routes.get('/v1/incidents', IncidentController.index)
+routes.delete('/v1/incidents/:id', IncidentController.remove)
 
-routes.get('/v1/profile', ProfileController.index);
+routes.get('/v1/profile', ProfileController.index)
 
-module.exports = routes;
+module.exports = routes
